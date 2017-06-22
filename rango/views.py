18fd3295ab98663bfceeb7f9e8028753159651af
NinjaOnRails <danny.hvu@gmail.com>
2017-06-22 17:44:39 +0200

    Used template inheritance and tags

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
+from django.core.urlresolvers import reverse
 
 def index(request):
     # Query the database for a list of ALL categories currently stored. # Order the categories by no. likes in descending order.
