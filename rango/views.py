0030b58d58739c6fb9d03349a32eca2c0ef80074
NinjaOnRails <danny.hvu@gmail.com>
2017-06-25 22:16:40 +0200

    fixed visits in views

--- rango/views.py
+++ rango/views.py
@@ -239,18 +239,17 @@ def visitor_cookie_handler(request):
     # If the cookie exists, the value returned is casted to an integer.
     # If the cookie doesn't exist, then the default value of 1 is used.
     visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_vist', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
 
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
 
     # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
+    if (datetime.now() - last_visit_time).seconds > 0:
         visits = visits + 1
         #update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
     else:
-        visits = 1
         # set the last visit cookie
         request.session['last_visit'] = last_visit_cookie
 
