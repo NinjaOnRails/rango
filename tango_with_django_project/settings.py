3422621c6231f6540215836db718e697663c7dc0
NinjaOnRails <danny.hvu@gmail.com>
2017-06-26 13:31:24 +0200

    Django Registration Redux used

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -150,4 +151,10 @@ MEDIA_URL = '/media/'
 
 # Login URL
 
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/accounts/login/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
